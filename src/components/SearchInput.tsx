4abc882063c1d4932bbb71c34240f5d71236d41a
Obasan Tolulope <obasantolu@gmail.com>
2025-09-17 22:39:52 +0100

    fixed navbar

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,21 +2,31 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import useGameStore from "../store";
-
-
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
-  const setSearchText = useGameStore(s => s.setSearchText);
-    const ref = useRef<HTMLInputElement>(null);
+  const setSearchText = useGameStore((s) => s.setSearchText);
+  const ref = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
   return (
-      <form onSubmit={(event) => {
-          event.preventDefault();
-          if (ref.current) setSearchText(ref.current.value);
-    }}>
-        <InputGroup>
-          <InputLeftElement children={<BsSearch />} />
-          <Input ref={ref} borderRadius={20} placeholder="Search games..." variant="filled" />
-        </InputGroup>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (ref.current) {
+          setSearchText(ref.current.value);
+          navigate("/");
+        }
+      }}
+    >
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />} />
+        <Input
+          ref={ref}
+          borderRadius={20}
+          placeholder="Search games..."
+          variant="filled"
+        />
+      </InputGroup>
     </form>
   );
 };
