6dcebbdf8dc96ee1c970cfe2dc554318ffe60eea
Obasan Tolulope <obasantolu@gmail.com>
2025-09-17 23:17:51 +0100

    debugging deployment

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,7 +4,7 @@ import GameCardContainer from "./GameCardContainer";
 import GameCardSkeleton from "./GameCardSkeleton";
 import React from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
-import useGames from "../hooks/useGames";
+import useGames from "../hooks/UseGames";
 
 const GameGrid = () => {
   const {
